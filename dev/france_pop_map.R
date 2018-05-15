0761337206971c3206ecd73b76c6c829eead4223
pvictor <perrier.victor@gmail.com>
2018-05-15 17:49:47 +0200

    examples +

---
+++ dev/france_pop_map.R
@@ -0,0 +1,49 @@
+
+#  ------------------------------------------------------------------------
+#
+# Title : France's population
+#    By : Victor
+#  Date : 2018-05-08
+#
+#  ------------------------------------------------------------------------
+
+
+
+# Packages ----------------------------------------------------------------
+
+library( r2d3maps )
+library( rnaturalearth )
+library( magrittr )
+library( dplyr )
+
+
+
+
+# Datas -------------------------------------------------------------------
+
+fr_dept <- ne_states(country = "france", returnclass = "sf")
+fr_dept <- fr_dept[fr_dept$type_en %in% "Metropolitan department", ]
+
+data(pop_fr)
+
+fr_dept <- left_join(
+  x = fr_dept,
+  y = pop_fr
+)
+
+
+
+
+
+# Map ---------------------------------------------------------------------
+
+r2d3map(shape = fr_dept) %>%
+  add_continuous_scale(var = "population_totale", palette = "Blues", n_breaks = 7) %>%
+  add_tooltip(value = "<b>{nom_du_departement}</b>: {population_totale}") %>%
+  add_legend(title = "Population", d3_format = ".2s") %>%
+  add_labs(title = "France population")
+
+
+
+
+
